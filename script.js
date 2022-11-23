41e51c80700fc4cacc5a8bb11d21cb1889bf3a5f
Dragibuslapin <118922952+Dragibuslapin@users.noreply.github.com>
2022-11-23 18:30:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+$(".btn_buffalo").on("click", function(){
+$(".detail_buffalo").addClass("open");
+});
+$(".close_buffalo").on("click", function(){
+    $(".detail_buffalo").removeClass("open");
+    });
