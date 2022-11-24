787d21baed72bb022748afc5a8b7ac2b6304e1e2
Dragibuslapin <118922952+Dragibuslapin@users.noreply.github.com>
2022-11-24 13:27:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ $(".detail_buffalo").addClass("open");
 $(".close_buffalo").on("click", function(){
     $(".detail_buffalo").removeClass("open");
     });
+
+    $(".btn_faceid").on("click", function(){
+        $(".detail_faceid").addClass("open");
+        });
+        $(".close_faceid").on("click", function(){
+            $(".detail_faceid").removeClass("open");
+            });
+
+            $(".btn_aquosus").on("click", function(){
+                $(".detail_aquosus").addClass("open");
+                });
+                $(".close_aquosus").on("click", function(){
+                    $(".detail_aquosus").removeClass("open");
+                    });
+
+
+                    $(".btn_aimemories").on("click", function(){
+                        $(".detail_aimemories").addClass("open");
+                        });
+                        $(".close_aimemories").on("click", function(){
+                            $(".detail_aimemories").removeClass("open");
+                            });
